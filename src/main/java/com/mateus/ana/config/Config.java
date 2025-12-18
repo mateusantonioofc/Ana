7a8a427dccd1e3e170cde433b448070b90331cf4
Mateu <mateusantonioofc@gmail.com>
2025-12-18 00:11:06 -0300

    listening config

---
+++ src/main/java/com/mateus/ana/config/Config.java
@@ -0,0 +1,17 @@
+package com.mateus.ana.config;
+
+public class Config {
+
+    public static final String MODEL_PATH = "models/pt/";
+    public static final float SAMPLE_RATE = 16000f;
+    public static final String[] WAKE_WORDS = {
+            "assistente",
+            "ana",
+            "hey ana",
+            "teste"
+    };
+
+    public static final boolean WAKE_WORD_ENABLED = true;
+
+    public static final long LISTEN_WINDOW_MS = 5000;
+}
