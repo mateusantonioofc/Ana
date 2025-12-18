77a1104fb1f387ad1c615ae8433ccc7e498ba026
Mateu <mateusantonioofc@gmail.com>
2025-12-18 00:11:45 -0300

    upgrade detect method

--- src/main/java/com/mateus/ana/modules/web/WebSearchModule.java
+++ src/main/java/com/mateus/ana/modules/web/WebSearchModule.java
@@ -8,12 +8,26 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.Normalizer;
 
 public class WebSearchModule implements CoreModule {
 
     @Override
     public boolean canHandle(String input) {
-        return input.toLowerCase().startsWith("pesquisar");
+        if (input == null || input.isEmpty()) return false;
+
+        String text = normalize(input);
+
+        return text.matches(".*\\b(procure|procurar|procura|buscar|busque|pesquisar|pesquise)\\b.*");
+    }
+
+    private String normalize(String text) {
+        return Normalizer.normalize(text, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
+                .toLowerCase()
+                .replaceAll("[^a-z ]", "")
+                .replaceAll("\\s+", " ")
+                .trim();
     }
 
     @Override
