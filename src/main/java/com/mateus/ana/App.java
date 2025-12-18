144903db1df308c707a85849eea845a469abe1f9
Mateu <mateusantonioofc@gmail.com>
2025-12-17 22:06:05 -0300

    Impl of modules

---
+++ src/main/java/com/mateus/ana/App.java
@@ -0,0 +1,13 @@
+package com.mateus.ana;
+
+import com.mateus.ana.core.Core;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Core modules = new Core();
+
+        modules.handle("pesquisar Java");
+    }
+}
