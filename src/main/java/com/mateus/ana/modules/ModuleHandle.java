78adf01e12308eeab39c0b2be78317578c09c0b7
Mateu <mateusantonioofc@gmail.com>
2025-12-17 22:06:34 -0300

    handle of modules

---
+++ src/main/java/com/mateus/ana/modules/ModuleHandle.java
@@ -0,0 +1,16 @@
+package com.mateus.ana.modules;
+
+import com.mateus.ana.modules.web.WebSearchModule;
+
+import java.util.List;
+
+public class ModuleHandle {
+
+    public ModuleHandle() {}
+
+    public static List<CoreModule<?>> getModules() {
+        return  List.of(
+                new WebSearchModule()
+        );
+    }
+}
