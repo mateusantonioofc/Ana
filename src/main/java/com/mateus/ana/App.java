c49a226e8a3f15a6e29ae251ceb91ea948eae76f
Mateu <mateusantonioofc@gmail.com>
2025-12-18 00:12:30 -0300

    impl listening module

--- src/main/java/com/mateus/ana/App.java
+++ src/main/java/com/mateus/ana/App.java
@@ -1,13 +1,31 @@
 package com.mateus.ana;
 
 import com.mateus.ana.core.Core;
+import com.mateus.ana.listening.SpeechListener;
+import com.mateus.ana.listening.SpeechModule;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
+
         Core modules = new Core();
+        SpeechModule speech = new SpeechModule();
 
-        modules.handle("pesquisar Java");
+        speech.setListener(new SpeechListener() {
+
+            @Override
+            public void onFinalResult(String text) {
+                System.out.println(text);
+
+                modules.handle(text);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        speech.start();
     }
 }
