8d8353b2f2f736d2242c57928b5ac976a2d21214
Mateu <mateusantonioofc@gmail.com>
2025-12-18 13:12:58 -0300

    start application

--- src/main/java/com/mateus/ana/App.java
+++ src/main/java/com/mateus/ana/App.java
@@ -1,31 +1,7 @@
 package com.mateus.ana;
 
-import com.mateus.ana.core.Core;
-import com.mateus.ana.listening.SpeechListener;
-import com.mateus.ana.listening.SpeechModule;
 
 public class App {
 
-    public static void main(String[] args) {
-
-        Core modules = new Core();
-        SpeechModule speech = new SpeechModule();
-
-        speech.setListener(new SpeechListener() {
-
-            @Override
-            public void onFinalResult(String text) {
-                System.out.println(text);
-
-                modules.handle(text);
-            }
-
-            @Override
-            public void onError(Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        speech.start();
-    }
+    public static void main(String[] args) { Conversation.start(); }
 }
